db3a5f96bb0b0f4c1b3a84757c51ab02bc1255ad
NiczinXD <nicolasxpinheiro@gmail.com>
2024-04-21 19:28:57 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Bem vindo ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('escolha um número entre 1 e 10');
+
+if (numeroSecreto == chute) {
+    console.log('Isso aí cara!Parabéns pro cê (5)')
+}
